fc53bfbed274ff36605223207ac81cc6cd967bf3
Persian Prince <persianpros@yahoo.com>
2019-02-08 11:58:04 +0330

    export symbols needed for e2procfs module by moham96 (#1)
    * Update aml_rtc.c
    * Update osd_hw.c

--- drivers/amlogic/display/osd/osd_hw.c
+++ drivers/amlogic/display/osd/osd_hw.c
@@ -921,12 +921,12 @@ void  osd_set_gbl_alpha_hw(u32 index, u32 gbl_alpha)
 		osd_wait_vsync_hw();
 	}
 }
-
+EXPORT_SYMBOL(osd_set_gbl_alpha_hw);
 u32 osd_get_gbl_alpha_hw(u32  index)
 {
 	return osd_hw.gbl_alpha[index];
 }
-
+EXPORT_SYMBOL(osd_get_gbl_alpha_hw);
 void osd_set_color_key_hw(u32 index, u32 color_index, u32 colorkey)
 {
 	u8 r = 0, g = 0, b = 0, a = (colorkey & 0xff000000) >> 24;
@@ -1266,7 +1266,7 @@ void osd_get_free_scale_enable_hw(u32 index, u32 *free_scale_enable)
 {
 	*free_scale_enable = osd_hw.free_scale_enable[index];
 }
-
+EXPORT_SYMBOL(osd_get_free_scale_enable_hw);
 void osd_set_free_scale_mode_hw(u32 index, u32 freescale_mode)
 {
 	osd_hw.free_scale_mode[index] = freescale_mode;
@@ -1276,7 +1276,7 @@ void osd_get_free_scale_mode_hw(u32 index, u32 *freescale_mode)
 {
 	*freescale_mode = osd_hw.free_scale_mode[index];
 }
-
+EXPORT_SYMBOL(osd_get_free_scale_mode_hw);
 void osd_set_4k2k_fb_mode_hw(u32 fb_for_4k2k)
 {
 	osd_hw.fb_for_4k2k = fb_for_4k2k;
@@ -1287,13 +1287,13 @@ void osd_get_free_scale_width_hw(u32 index, u32 *free_scale_width)
 	*free_scale_width = osd_hw.free_src_data[index].x_end -
 		osd_hw.free_src_data[index].x_start + 1;
 }
-
+EXPORT_SYMBOL(osd_get_free_scale_width_hw);
 void osd_get_free_scale_height_hw(u32 index, u32 *free_scale_height)
 {
 	*free_scale_height = osd_hw.free_src_data[index].y_end -
 		osd_hw.free_src_data[index].y_start + 1;
 }
-
+EXPORT_SYMBOL(osd_get_free_scale_height_hw);
 void osd_get_free_scale_axis_hw(u32 index, s32 *x0, s32 *y0, s32 *x1, s32 *y1)
 {
 	*x0 = osd_hw.free_src_data[index].x_start;
@@ -1301,7 +1301,7 @@ void osd_get_free_scale_axis_hw(u32 index, s32 *x0, s32 *y0, s32 *x1, s32 *y1)
 	*x1 = osd_hw.free_src_data[index].x_end;
 	*y1 = osd_hw.free_src_data[index].y_end;
 }
-
+EXPORT_SYMBOL(osd_get_free_scale_axis_hw);
 void osd_set_free_scale_axis_hw(u32 index, s32 x0, s32 y0, s32 x1, s32 y1)
 {
 	osd_hw.free_src_data[index].x_start = x0;
@@ -1317,7 +1317,7 @@ void osd_get_scale_axis_hw(u32 index, s32 *x0, s32 *y0, s32 *x1, s32 *y1)
 	*y0 = osd_hw.scaledata[index].y_start;
 	*y1 = osd_hw.scaledata[index].y_end;
 }
-
+EXPORT_SYMBOL(osd_get_scale_axis_hw);
 void osd_set_scale_axis_hw(u32 index, s32 x0, s32 y0, s32 x1, s32 y1)
 {
 	osd_hw.scaledata[index].x_start = x0;
@@ -1357,7 +1357,7 @@ void osd_get_window_axis_hw(u32 index, s32 *x0, s32 *y0, s32 *x1, s32 *y1)
 	*x0 = osd_hw.free_dst_data[index].x_start;
 	*x1 = osd_hw.free_dst_data[index].x_end;
 }
-
+EXPORT_SYMBOL(osd_get_window_axis_hw);
 void osd_set_window_axis_hw(u32 index, s32 x0, s32 y0, s32 x1, s32 y1)
 {
 	const struct vinfo_s *vinfo;
--- drivers/amlogic/rtc/aml_rtc.c
+++ drivers/amlogic/rtc/aml_rtc.c
@@ -448,7 +448,7 @@ static unsigned int ser_access_read(unsigned long addr)
 
 	return val;
 }
-
+EXPORT_SYMBOL(ser_access_read);
 static int ser_access_write(unsigned long addr, unsigned long data)
 {
 	unsigned long flags;
@@ -460,7 +460,7 @@ static int ser_access_write(unsigned long addr, unsigned long data)
 
 	return 0;
 }
-
+EXPORT_SYMBOL(ser_access_write);
 /***************************************************************************/
 int rtc_reset_gpo(struct device *dev, unsigned level)
 {
@@ -530,7 +530,7 @@ int rtc_set_alarm_aml(struct device *dev, struct alarm_data_t *alarm_data)
 
 	return 0;
 }
-
+EXPORT_SYMBOL(rtc_set_alarm_aml);
 /*************************************************************************/
 
 
